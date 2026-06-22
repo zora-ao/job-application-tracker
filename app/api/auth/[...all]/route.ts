d3cab4a6f92049dda14679a50c06035980db4a47
Carl Jasper Ramos <zoraao79@gmail.com>
2026-06-22 15:26:40 +0800

    fixing

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,16 +1,4 @@
 import { auth } from "@/lib/auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
-import { NextRequest } from "next/server";
 
-const authHandlers = toNextJsHandler(auth);
-
-export async function GET(request: NextRequest) {
-  // 💡 Accessing request properties tells Next.js this route cannot be pre-rendered statically
-  const url = request.url; 
-  return await authHandlers.GET(request);
-}
-
-export async function POST(request: NextRequest) {
-  const url = request.url;
-  return await authHandlers.POST(request);
-}
+export const { GET, POST } = toNextJsHandler(auth);
